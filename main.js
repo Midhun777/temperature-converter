faa280c26f7a3bd943211d506c85115159fd3670
Midhun777 <midmat4828@gmail.com>
2024-03-07 20:46:14 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,39 +1,47 @@
 var textBox = document.getElementById('textBox');
-
 var toCelcius = document.getElementById('toCelcius');
-
 var toFahrenheit = document.getElementById('toFahrenheit');
-
 var result = document.getElementById('result');
 
 var tempVal;
-
 var temp;
 
 function convert() {
-
 	if (toCelcius.checked) {
-
 		tempVal = Number(textBox.value);
 		temp = ((tempVal - 32) * (5 / 9));
-		
 		result.innerHTML = temp.toFixed(1) + '°C';
-
 	}
 
 	else if (toFahrenheit.checked) {
-
 		tempVal = Number(textBox.value);
 		temp = ((tempVal * (9 / 5)) + 32);
-		
 		result.innerHTML = temp.toFixed(1) + '°F';
-
 	}
 
 	else {
-
 		result.innerHTML = 'Select an option!';
+	}
+}
 
+/*
+
+function convert() {
+	if (toCelcius.checked) {
+		tempVal = Number(textBox.value);
+		temp = ((tempVal - 32) * (5 / 9));
+		result.innerHTML = temp.toFixed(1) + '°C';
 	}
 
+	else if (toFahrenheit.checked) {
+		tempVal = Number(textBox.value);
+		temp = ((tempVal * (9 / 5)) + 32);
+		result.innerHTML = temp.toFixed(1) + '°F';
+	}
+
+	else {
+		result.innerHTML = 'Select an option!';
+	}
 }
+
+*/
